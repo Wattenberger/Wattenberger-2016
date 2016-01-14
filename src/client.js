e1a85f2fee7805c63773470ac89995698e6eb578
wattenberger <wattenberger@gmail.com>
2016-01-13 20:57:04 -0600

    syntax error

--- src/client.js
+++ src/client.js
@@ -8,7 +8,7 @@ import createStore from "./store"
 if (__DEV__) window.Perf = require('react-addons-perf')
 
 const store = createStore()
-const appRoot = document.getElementById('app
+const appRoot = document.getElementById('app')
 
 ReactDOM.render((
   <Provider store={store}>
