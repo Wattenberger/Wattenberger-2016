f0265034c5b396cdfd75e8a8ed297af2c724b10d
Amelia <wattenberger@gmail.com>
2017-01-04 21:52:55 -0500

    sketch Day 3

--- src/components/Sketches/list.jsx
+++ src/components/Sketches/list.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 import Day1 from "./sketches/Day1"
 import Day2 from "./sketches/Day2"
+import Day3 from "./sketches/Day3"
 
 export const list = [
   Day1,
   Day2,
+  Day3,
 ]
---
+++ src/components/Sketches/sketches/Day3.jsx
@@ -0,0 +1,141 @@
+import React, {Component} from "react"
+import classNames from "classnames"
+
+require('./Day3.scss')
+
+let interval
+const INTERVAL_LENGTH = 90
+const ARC_SEGMENTS = 10
+
+class Day3 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      canvas: null,
+      point: null,
+      clockwise: true,
+    }
+  }
+
+  componentDidMount() {
+    let canvas = this.canvas.getContext("2d")
+    this.setState({canvas})
+    interval = window.setInterval(this.draw, INTERVAL_LENGTH)
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(interval)
+  }
+
+  getClassName() {
+    return classNames("Day3")
+  }
+
+  draw = () => {
+    let {canvas, point} = this.state
+    if (!canvas) return
+
+    // canvas.clearRect(0, 0, width, height)
+    if (point && point.perc < 1) {
+      this.continueArc()
+    } else {
+      this.startNewPoint()
+    }
+  }
+
+  startNewPoint() {
+    let {canvas, point, clockwise} = this.state
+    let {width, height} = canvas.canvas
+    point = point || {x: width / 2, y: height / 2}
+
+    let newPoint = {
+      x: _.random(point.x < 20 ? 0 : -20, point.x > width - 20 ? 0 : 20) + point.x,
+      y: _.random(point.y < 20 ? 0 : -20, point.y > height - 20 ? 0 : 20) + point.y,
+    }
+
+    const l = newPoint.x - point.x
+    const h = newPoint.y - point.y
+    const r = Math.sqrt(Math.abs(h*h) + Math.abs(l*l)) / 2
+    const startAngle = Math.tanh(h / l)
+
+    _.extend(newPoint, {
+      l, h, startAngle, r: r, perc: 0
+    })
+
+    this.drawDot(newPoint, "#45aeb1")
+    this.setState({point: newPoint, clockwise: !clockwise})
+  }
+
+  fadeCanvas(pct) {
+    let {canvas} = this.state
+    let {width, height} = canvas.canvas
+
+    canvas.fillStyle = `rgba(255, 255, 255, ${pct})`
+    canvas.fillRect(0, 0, width, height)
+  }
+
+  drawDot(point, color) {
+    let {canvas} = this.state
+    canvas.fillStyle = color
+    this.createPathMethods([
+      {
+        type: "arc",
+        args: [point.x, point.y, 2, 0, Math.PI * 2, false]
+      },
+    ], canvas)
+    canvas.fill()
+  }
+
+  continueArc() {
+    let {canvas, point} = this.state
+    let endAngle = point.startAngle - Math.PI * point.perc
+
+    canvas.strokeStyle = "rgba(0, 0, 0, 0.4)"
+    this.createPathMethods([
+      {
+        type: "arc",
+        // arc(x, y, radius, startAngle, endAngle, anticlockwise)
+        args: [point.x + point.l / 2, point.y + point.h / 2, point.r, point.startAngle, endAngle, true]
+      },
+    ], canvas)
+    canvas.stroke()
+    point.perc += 1 / ARC_SEGMENTS
+
+    this.fadeCanvas(0.01)
+  }
+
+  createPathMethods(points, canvas) {
+    canvas.beginPath()
+    let moveTo
+    points.map((point, idx) => {
+      let method = point.type || (moveTo || !idx ? "moveTo" : "lineTo")
+      canvas[method](...point.args)
+      moveTo = !!point.type
+    })
+  }
+
+  drawPath() {
+    let {canvas} = this.state
+
+    let points = _.times(3, i => ({
+      args: [
+        _.random(canvas.canvas.width),
+        _.random(canvas.canvas.height),
+      ]
+    }))
+    this.createPathMethods(points, canvas)
+    canvas.fill()
+  }
+
+  render() {
+    return (
+      <div className={this.getClassName()}>
+        <canvas
+          ref={canvas => this.canvas = canvas}
+        />
+      </div>
+    )
+  }
+}
+
+export default Day3
