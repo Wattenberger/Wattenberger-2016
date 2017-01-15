00684376084b522d8e82b818744cdf6bcc28eab4
wattenberger <wattenberger@gmail.com>
2017-01-15 11:48:38 -0500

    cleanup

--- src/server/render.js
+++ src/server/render.js
@@ -1,6 +1,4 @@
 // Express middleware to render the app server-side and expose its state to the client
-import React from "react"
-
 function renderStaticHTML(req, res, next) {
   try {
     res.sendFile(`${__dirname}/index.html`)
