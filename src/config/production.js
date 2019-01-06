d9ed83e4be7ddd9ff4fd99f04eae7cf54b3b361c
Wattenberger <wattenberger@gmail.com>
2019-01-06 11:48:07 -0500

    move dev dependencies

--- src/config/production.js
+++ src/config/production.js
@@ -1,3 +1,3 @@
 exports.WEBPACK_HOST = process.env.HOST               || "wattenberger.com"
-exports.WEBPACK_PORT = parseInt(process.env.PORT)     || 8000
+exports.WEBPACK_PORT = parseInt(process.env.PORT)     || 8080
 exports.STATIC_HOST  = process.env.STATIC_HOST        || exports.WEBPACK_HOST
