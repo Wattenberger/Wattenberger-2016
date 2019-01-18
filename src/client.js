3c8c2620797d98e830d416496c4dcd816949e07e
Wattenberger <wattenberger@gmail.com>
2019-01-18 01:01:00 -0500

    add intersectionobserver polyfill

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 import "@babel/register"
 // import "@babel/polyfill"
+require('intersection-observer');
+
 import React from "react"
 import ReactDOM from "react-dom"
 import { AppContainer } from 'react-hot-loader'
