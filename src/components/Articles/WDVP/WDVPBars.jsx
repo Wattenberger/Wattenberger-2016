e9c3818bceb6f761e5104289bacaea378debf264
Wattenberger <wattenberger@gmail.com>
2019-01-26 21:07:28 -0500

    wdvp bars - animate zoom?

--- src/components/Articles/WDVP/WDVPBars.jsx
+++ src/components/Articles/WDVP/WDVPBars.jsx
@@ -632,9 +632,9 @@ class WDVPBarsChart extends PureComponent {
         .onUpdate(() => {
           this.camera.zoom = zoomTweenAmount.zoom
           this.camera.updateProjectionMatrix()
+          this.animate()
         })
         .start()
-        .onComplete(this.renderScene)
 
   }
 
