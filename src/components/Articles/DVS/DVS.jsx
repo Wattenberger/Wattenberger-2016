7f3ccc0f19ebd34c94e1898c2ac24f3a86b6ccfb
Wattenberger <wattenberger@gmail.com>
2019-03-12 22:57:08 -0400

    dvs - move animation delay

--- src/components/Articles/DVS/DVS.jsx
+++ src/components/Articles/DVS/DVS.jsx
@@ -22,6 +22,30 @@ const formatNumberWithDecimal = d => numeral(d).format("0,0.0a")
 const ordinalColors = ["#c7ecee", "#778beb", "#f7d794", "#63cdda", "#cf6a87", "#e77f67", "#786fa6", "#FDA7DF", "#4b7bec", "#778ca3"];
 const colorScale = d3.scaleLinear().range(["#c7ecee", "#686de0"]).domain([0, 1])
 
+const axes = [
+  "society",
+  "visualization",
+  "data",
+]
+const colorAxisScale = d3.scaleLinear()
+  .domain([0, 5])
+  .range([0, 1])
+const colors = [
+  d3.interpolateRainbow(0.43),
+  d3.interpolateRainbow(0.76),
+  d3.interpolateRainbow(0.1),
+]
+const grabColor = (values) => {
+  const valueIndices = _.map(axes, (axis, i) => [
+    i,
+    values[i]
+  ])
+  const mainAxis = _.maxBy(valueIndices, "1")
+  return colorAxisScales[mainAxis[0]](mainAxis[1])
+}
+const colorAxisScales = colors.map(color => (
+  d3.interpolateHsl("#e2e4e7", color)
+))
 const dateAccessor = d => d3.timeParse("%m/%d/%Y")(d.date)
 const DVS = () => {
   const [data, setData] = useState([])
@@ -29,7 +53,10 @@ const DVS = () => {
 
   useEffect(() => {
     d3.csv(dataFile).then(rows => {
-      const sortedData = _.orderBy(rows, dateAccessor, "asc")
+      const sortedData = _.orderBy(_.map(rows, d => ({
+        ...d,
+        color: grabColor(_.map(axes, axis => colorAxisScale(+d[axis] || 0))),
+      })), dateAccessor, "asc")
       setData(sortedData)
     })
   }, [])
@@ -68,22 +95,6 @@ const DVS = () => {
 export default DVS
 
 
-const axes = [
-  "society",
-  "visualization",
-  "data",
-]
-const colorAxisScale = d3.scaleLinear()
-  .domain([0, 5])
-  .range([0, 1])
-const colors = [
-  d3.interpolateRainbow(0.43),
-  d3.interpolateRainbow(0.76),
-  d3.interpolateRainbow(0.1),
-]
-const colorAxisScales = colors.map(color => (
-  d3.interpolateHsl("#e2e4e7", color)
-))
 const cities = [
   ["Amsterdam", "Netherlands", 52, 4],
   ["Bangkok", "Thailand", 13, 100],
@@ -134,81 +145,74 @@ const cities = [
 //   name: "Tokyo",
 //   location: [35.6895, 139.6917],
 // }]
-const grabColor = (values) => {
-  const colors = values.map((d, i) => ({
-    color: d3.hsl(d3.color(colorAxisScales[i](d))),
-    value: d,
-  }))
-  const mainColor = _.maxBy(colors, "value").color
-  let combinedColor = d3.lab("gray")
-  combinedColor.l = d3.mean(colors, d => d.l)
-  combinedColor.a = d3.mean(colors, d => d.a)
-  combinedColor.b = d3.mean(colors, d => d.b)
-  return mainColor.toString()
-}
 const logScaleDistance = d => d ? Math.log10(d) * 150 : 0
 const DVSMap = ({ data=[], location }) => {
-  const dateAccessor = d => d3.timeParse("%m/%d/%Y")(d.date)
-  const dateScale = d3.scaleLog()
-    .domain(d3.extent(data, dateAccessor))
-    .range([0, 300])
-
-  const relativeLocations = _.map(data, d => ({
-      ...d,
-      newLocation: findNewPoint(
-        0,
-        0,
-        findAngle(wrapAroundLongitude(location[1], +d.long), +d.lat - location[0]),
-        logScaleDistance(findDistance(wrapAroundLongitude(location[1], +d.long), +d.lat - location[0])),
-      ),
-      color: grabColor(_.map(axes, axis => colorAxisScale(+d[axis] || 0))),
-  }))
-
-  const onMouseOver = d => () => {
-    console.log(d)
-  }
-
-  const enhancedCities = cities.map(city => {
-    const baseDistance = findDistance(wrapAroundLongitude(location[1], city[3]), city[2] - location[0])
-    const angle = findAngle(wrapAroundLongitude(location[1], city[3]), city[2] - location[0])
-    const newLocation = findNewPoint(
-      0,
-      0,
-      angle,
-      logScaleDistance(baseDistance) + 50,
-    )
-
-    return {
-      angle,
-      location: newLocation,
-      baseDistance: baseDistance,
-      name: `${ city[0] }, ${ city[1] }`,
-      city: city[0],
-      country: city[1],
-    }
+  const [state, setState] = useState({
+    thisCity: null,
+    cities: [],
+    dots: [],
   })
 
-  const numAngleSteps = 10
-  const angleSteps = (Math.PI * 2) / (numAngleSteps- 1)
-  const anglePadding = angleSteps * 0.1
-  const angleCities = _.filter(_.map(_.times(numAngleSteps), i => {
-    const startAngle = angleSteps * i - Math.PI + anglePadding
-    const endAngle = angleSteps * (i + 1) - Math.PI - anglePadding
-    const city = _.find(enhancedCities, d => d.baseDistance > 50 && d.angle > startAngle && d.angle < endAngle)
-    return city
-  }), d => d)
+  useEffect(() => {
+      const relativeLocations = _.map(data, d => ({
+          ...d,
+          location: findNewPoint(
+            0,
+            0,
+            findAngle(wrapAroundLongitude(location[1], +d.long), +d.lat - location[0]),
+            logScaleDistance(findDistance(wrapAroundLongitude(location[1], +d.long), +d.lat - location[0])),
+          ),
+      }))
 
-  const thisCity = _.find(enhancedCities, d => d.baseDistance < 2)
+      const enhancedCities = cities.map(city => {
+        const baseDistance = findDistance(wrapAroundLongitude(location[1], city[3]), city[2] - location[0])
+        const angle = findAngle(wrapAroundLongitude(location[1], city[3]), city[2] - location[0])
+        const newLocation = findNewPoint(
+          0,
+          0,
+          angle,
+          logScaleDistance(baseDistance) + 50,
+        )
+
+        return {
+          angle,
+          location: newLocation,
+          baseDistance: baseDistance,
+          name: `${ city[0] }, ${ city[1] }`,
+          city: city[0],
+          country: city[1],
+        }
+      })
+
+      const numAngleSteps = 10
+      const angleSteps = (Math.PI * 2) / (numAngleSteps- 1)
+      const anglePadding = angleSteps * 0.1
+      const angleCities = _.filter(_.map(_.times(numAngleSteps), i => {
+        const startAngle = angleSteps * i - Math.PI + anglePadding
+        const endAngle = angleSteps * (i + 1) - Math.PI - anglePadding
+        const city = _.find(enhancedCities, d => d.baseDistance > 50 && d.angle > startAngle && d.angle < endAngle)
+        return city
+      }), d => d)
+
+      const thisCity = _.find(enhancedCities, d => d.baseDistance < 2)
+
+      setState({
+        thisCity,
+        cities: angleCities,
+        dots: relativeLocations,
+      })
+    }, [data, location])
+
 
   return (
     <div className="DVSMap">
       <div className="DVSMap__center">
         <div className="DVSMap__center__text">
-        { thisCity && thisCity.city }
+        { state.thisCity && state.thisCity.city }
         </div>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
       </div>
-      {_.map(angleCities, (d, i) => (
+      {_.map(state.cities, (d, i) => (
         <div
           className="DVSMap__location"
           key={d.name}
@@ -223,19 +227,28 @@ const DVSMap = ({ data=[], location }) => {
           <div className="DVSMap__location__country">{ d.country }</div>
         </div>
       ))}
-      {_.map(relativeLocations, (d, i) => (
-        <div
-          className="DVSMap__dot"
+      {_.map(state.dots, (d, i) => (
+        <DvsMapDot
           key={i}
-          style={{
-            background: d.color,
-            transform: `translate(${d.newLocation[0]}px, ${d.newLocation[1] * -1}px)`,
-          }}
+          i={i}
+          color={d.color}
+          location={d.location}
         />
       ))}
     </div>
   )
 }
+
+const DvsMapDot = React.memo(({ color, location, i }) => (
+  <div
+    className="DVSMap__dot"
+    style={{
+      background: color,
+      transform: `translate(${location[0]}px, ${location[1] * -1}px)`,
+      animationDelay: `${i * 0.003}s`,
+    }}
+  />
+))
 
 const findAngle = (xDelta, yDelta) => (
   Math.atan2(yDelta, xDelta)
